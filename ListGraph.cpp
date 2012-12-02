fe81bbb1ce6f26971ce5c6648358d1ea795e4926
Joshua Ruebusch <ruebusjw@muohio.edu>
2012-12-02 16:28:22 -0500

    ListGraph fully functional

--- ListGraph.cpp
+++ ListGraph.cpp
@@ -15,20 +15,37 @@ ListGraph::~ListGraph(){
 }
 
 void ListGraph::addEdge(NodeID u, NodeID v, EdgeWeight weight){
-
+	if(0 <= u < edgeList.size() && 0 <= v < edgeList.size() && u != v && weight > 0)
+	{
+		edgeList.at(u).push_back(NWPair(v, weight));
+		edgeList.at(v).push_back(NWPair(u, weight));
+		num_edges++;
+	}
 }
 
+//from class notes
 EdgeWeight ListGraph::weight(NodeID u, NodeID v) const{
+	if(0 <= u < edgeList.size() && 0 <= v < edgeList.size())
+	{
+		EList::const_iterator it;
+		for(it = edgeList.at(u).begin(); it !=edgeList.at(u).end(); it++)
+		{
+			NWPair tp = *it;
+			if(tp.first == v)
+				return tp.second;
+		}
+	}
 	return 0;
 }
 
 std::list<NWPair> ListGraph::getAdj(NodeID u) const{
-	std::list<NWPair> EList;
-	return EList;
+	if(0 <= u < edgeList.size())
+		return edgeList.at(u);
 }
 
 unsigned ListGraph::degree(NodeID u) const{
-	return 0;
+	if(0 <= u < edgeList.size())
+		return getAdj(u).size();
 }
 
 unsigned ListGraph::size() const{
--- MatrixGraph.cpp
+++ MatrixGraph.cpp
@@ -4,12 +4,12 @@ MatrixGraph::MatrixGraph(unsigned num_nodes){
 
 	num_edges = 0;
 
-	M.resize(num_nodes);
+	this->M.resize(num_nodes);
 
 	for(int i = 0; i < num_nodes; i++){
-		M.resize(num_nodes);
+		this->M.resize(num_nodes);
 		for(int j = 0; j < num_nodes; j++){
-			M.at(i).push_back(0);
+			this->M.at(i).push_back(0);
 		}
 	}
 }
@@ -20,28 +20,28 @@ MatrixGraph::~MatrixGraph(){
 
 void MatrixGraph::addEdge(NodeID u, NodeID v, EdgeWeight weight){
 	//test preconditions as mentioned in Graph.h
-	if(0 <= u && u < M.size() && 0 <= v && v < M.size() && u != v && M.at(u).at(v) == 0 && M.at(v).at(u) == 0 && weight > 0)
+	if(0 <= u < M.size() && 0 <= v < M.size() && u != v && M.at(u).at(v) == 0 && M.at(v).at(u) == 0 && weight > 0)
 	{
 		num_edges++; //increment
-		M.at(u).at(v) = M.at(v).at(u) = weight; //assign weights
+		this->M.at(u).at(v) = this->M.at(v).at(u) = weight; //assign weights
 	}
 }
 
 EdgeWeight MatrixGraph::weight(NodeID u, NodeID v) const{
 	//preconditions as mentioned in Graph.h
-	if(0 <= u && u < M.size() && 0 <= v && v < M.size())
-		return M.at(u).at(v);
+	if(0 <= u < this->M.size() && 0 <= v < this->M.size())
+		return this->M.at(u).at(v);
 	else
 		return 0; // return 0 if non-existant points
 }
 
 std::list<NWPair> MatrixGraph::getAdj(NodeID u) const{
-	if(0 <= u && u < M.size())	//precondition as mentioned in Graph.h
+	if(0 <= u < this->M.size())	//precondition as mentioned in Graph.h
 	{
 		std::list<NWPair> EList;
-		for(int i = 0; i < M.at(u).size(); i++)
+		for(int i = 0; i < this->M.at(u).size(); i++)
 		{
-			NWPair cur(i, M.at(u).at(i));
+			NWPair cur(i, this->M.at(u).at(i));
 			if(cur.second != 0)				//weight (second) will only be 0 if it has not been assigned. 
 				EList.push_back(cur);
 		}
@@ -50,17 +50,17 @@ std::list<NWPair> MatrixGraph::getAdj(NodeID u) const{
 }
 
 unsigned MatrixGraph::degree(NodeID u) const{
-	if(0 <= u && u < M.size())
+	if(0 <= u < this->M.size())
 	{
-		return getAdj(u).size();	//when conditions met simply return size of adjacency list
+		return this->getAdj(u).size();	//when conditions met simply return size of adjacency list
 	}
 }
 
 unsigned MatrixGraph::size() const{
-	return M.size();				//just return matrix size
+	return this->M.size();				//just return matrix size
 }
 
 unsigned MatrixGraph::numEdges() const{
-	return num_edges;				//return number of edges 
+	return this->num_edges;				//return number of edges 
 }
 
