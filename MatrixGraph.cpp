30f0e7fe9ba1a2c290c5070b1df9e6ed7b8dec7a
Joshua Ruebusch <ruebusjw@muohio.edu>
2012-12-02 13:55:51 -0500

    MatrixGraph fully functional

--- MatrixGraph.cpp
+++ MatrixGraph.cpp
@@ -20,7 +20,7 @@ MatrixGraph::~MatrixGraph(){
 
 void MatrixGraph::addEdge(NodeID u, NodeID v, EdgeWeight weight){
 	//test preconditions as mentioned in Graph.h
-	if(0 <= u && u < M.size() && 0 <= v && v < M.size() && u != v && M.at(u).at(v) == 0 && M.at(v).at(u) == 0 && weight < 0)
+	if(0 <= u && u < M.size() && 0 <= v && v < M.size() && u != v && M.at(u).at(v) == 0 && M.at(v).at(u) == 0 && weight > 0)
 	{
 		num_edges++; //increment
 		M.at(u).at(v) = M.at(v).at(u) = weight; //assign weights
@@ -29,28 +29,38 @@ void MatrixGraph::addEdge(NodeID u, NodeID v, EdgeWeight weight){
 
 EdgeWeight MatrixGraph::weight(NodeID u, NodeID v) const{
 	//preconditions as mentioned in Graph.h
-	if(0 <= u < M.size(), 0 <= v < M.size())
+	if(0 <= u && u < M.size() && 0 <= v && v < M.size())
 		return M.at(u).at(v);
 	else
 		return 0; // return 0 if non-existant points
 }
 
-//stub
 std::list<NWPair> MatrixGraph::getAdj(NodeID u) const{
-	std::list<NWPair> EList;
-	return EList;
+	if(0 <= u && u < M.size())	//precondition as mentioned in Graph.h
+	{
+		std::list<NWPair> EList;
+		for(int i = 0; i < M.at(u).size(); i++)
+		{
+			NWPair cur(i, M.at(u).at(i));
+			if(cur.second != 0)				//weight (second) will only be 0 if it has not been assigned. 
+				EList.push_back(cur);
+		}
+		return EList;
+	}
 }
 
-//stub
 unsigned MatrixGraph::degree(NodeID u) const{
-	return 0;
+	if(0 <= u && u < M.size())
+	{
+		return getAdj(u).size();	//when conditions met simply return size of adjacency list
+	}
 }
 
 unsigned MatrixGraph::size() const{
-	return M.size();
+	return M.size();				//just return matrix size
 }
 
 unsigned MatrixGraph::numEdges() const{
-	return num_edges;
+	return num_edges;				//return number of edges 
 }
 
